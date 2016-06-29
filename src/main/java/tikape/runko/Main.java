e80142c533a96d5159a7a4f6a617f8a859d02dc7
Laura Kauria <Laura@1BQQ4X4.keskusta.dhcp.hupnet.helsinki.fi>
2016-06-29 15:52:48 +0300

    Herokuversio

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -22,7 +22,14 @@ public class Main {
             port(Integer.valueOf(System.getenv("PORT")));
         }
         
-        Database database = new Database("jdbc:sqlite:keskustelualue.db");
+        // käytetään oletuksena paikallista sqlite-tietokantaa
+        String jdbcOsoite = "jdbc:sqlite:keskustelualue.db";
+        // jos heroku antaa käyttöömme tietokantaosoitteen, otetaan se käyttöön
+        if (System.getenv("DATABASE_URL") != null) {
+            jdbcOsoite = System.getenv("DATABASE_URL");
+        } 
+
+        Database database = new Database(jdbcOsoite);
 
         AlueDao alueDao = new AlueDao(database);
         KeskusteluDao keskusteluDao = new KeskusteluDao(database);
--- src/main/java/tikape/runko/database/Database.java
+++ src/main/java/tikape/runko/database/Database.java
@@ -3,6 +3,7 @@ package tikape.runko.database;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.net.*;
 
 public class Database {
 
@@ -10,15 +11,18 @@ public class Database {
 
     public Database(String databaseAddress) throws ClassNotFoundException {
         this.databaseAddress = databaseAddress;
+        init();
     }
-
-    public Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(databaseAddress);
-    }
-
+  
     public void init() {
         List<String> lauseet = sqliteLauseet();
 
+        if (this.databaseAddress.contains("postgres")) {
+            lauseet = postgreLauseet();
+        } else {
+            lauseet = sqliteLauseet();
+        }    
+        
         // "try with resources" sulkee resurssin automaattisesti lopuksi
         try (Connection conn = getConnection()) {
             Statement st = conn.createStatement();
@@ -35,6 +39,42 @@ public class Database {
         }
     }
 
+    
+    public Connection getConnection() throws SQLException {
+        if (this.databaseAddress.contains("postgres")) {
+            try {
+                URI dbUri = new URI(databaseAddress);
+
+                String username = dbUri.getUserInfo().split(":")[0];
+                String password = dbUri.getUserInfo().split(":")[1];
+                String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+                return DriverManager.getConnection(dbUrl, username, password);
+            } catch (Throwable t) {
+                System.out.println("Error: " + t.getMessage());
+                t.printStackTrace();
+            }
+        }
+        return DriverManager.getConnection(databaseAddress);
+    }
+    
+     private List<String> postgreLauseet() {
+        ArrayList<String> lista = new ArrayList<>();
+
+        // tietokantataulujen luomiseen tarvittavat komennot suoritusjärjestyksessä
+        lista.add("DROP TABLE Alue;");
+        // heroku käyttää SERIAL-avainsanaa uuden tunnuksen automaattiseen luomiseen
+        lista.add("CREATE TABLE Alue ((id SERIAL PRIMARY KEY, nimi varchar(100)));");
+        
+        lista.add("DROP TABLE Keskustelu;");
+        lista.add("CREATE TABLE Keskustelu (id SERIAL PRIMARY KEY, aihe varchar(100) NOT NULL, alue integer NOT NULL, FOREIGN KEY(alue) REFERENCES Alue(id));");
+        
+        lista.add("DROP TABLE Viesti;");
+        lista.add("CREATE TABLE Viesti ((id integer PRIMARY KEY, aika timestamp NOT NULL, sisalto varchar(5000) NOT NULL, nimimerkki varchar(100) NOT NULL, keskustelu integer NOT NULL, FOREIGN KEY(keskustelu) REFERENCES Keskustelu(id));");
+
+        return lista;
+    }
+    
     private List<String> sqliteLauseet() {
         ArrayList<String> lista = new ArrayList<>();
 
